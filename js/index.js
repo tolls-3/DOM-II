f95da8aca6f94778beb02f0e2f6a0328c22a4c06
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-03 12:48:55 +0100

    Applied mouse over on Nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 // Your code goes here
+let test = document.querySelectorAll('.nav-link');
+
+test[0].addEventListener("mouseover", function( event ) {   
+    // highlight the mouseover target
+  event.target.style.color = "orange";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+test[1].addEventListener("mouseover", function( event ) {   
+    // highlight the mouseover target
+  event.target.style.color = "orange";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+test[2].addEventListener("mouseover", function( event ) {   
+    // highlight the mouseover target
+  event.target.style.color = "orange";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+test[3].addEventListener("mouseover", function( event ) {   
+    // highlight the mouseover target
+  event.target.style.color = "orange";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
